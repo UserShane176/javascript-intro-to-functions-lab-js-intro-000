6c4be5bf110c4719388c0362cd4a934948b7caea
Min-Hsuan Hung <poxuan33@gmail.com>
2018-06-22 12:00:58 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -44,8 +44,8 @@ describe('logWhisper(string)', function() {
 })
 function sayHiToGrandma(string){
 if (string === string.toLowerCase()) {return "I can't hear you"}
+else if (string === "I love you, Grandma.") {return "I love you, too."}
 else if (string === string.toUpperCase()) {return "YES INDEED!"}
-else (string === "I love you, Grandma.") {return "I love you, too."}
 }
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
